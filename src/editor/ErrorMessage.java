f1a1c0fffa83963c0a7674ffd3022d209fb922fe
Raphael Bucher <bucherraphael@gmx.ch>
2016-11-30 16:29:01 +0100

    Error message rendering beautified

--- src/editor/ErrorMessage.java
+++ src/editor/ErrorMessage.java
@@ -8,6 +8,7 @@ package editor;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics2D;
 
 /** An error message which is displayed in the middle of the drawable Panel. */
@@ -22,19 +23,39 @@ public class ErrorMessage {
     messageCreationTimeStamp = System.currentTimeMillis();
   }
 
+  // paints the message with a fading effect
   public static void paint(Graphics2D graphics2D) {
     if (!message.equals("")) {
-      // fade effect
-      graphics2D.setColor(new Color(255, 0, 0, getFadeAlpha()));
-
-      graphics2D.setFont(new Font("Arial", Font.PLAIN, Config.ERROR_MESSAGE_FONT_SIZE));
+      // Calculate the middle of the drawable Area
       Dimension drawableArea = graphics2D.getClipBounds().getSize();
-      // Render the String centered
-      graphics2D.drawString(message,
-          (int) drawableArea.getWidth() / 2 - message.length() * Config.ERROR_MESSAGE_FONT_SIZE / 5,
-          (int) drawableArea.getHeight() / 2 - Config.ERROR_MESSAGE_FONT_SIZE / 2);
+      int middleX = (int)drawableArea.getWidth() / 2;
+      int middleY = (int)drawableArea.getHeight() / 2;
       
-      graphics2D.setColor(Color.BLACK);
+      // Font metric calculations
+      Font messageFont = new Font("Arial", Font.PLAIN, Config.ERROR_MESSAGE_FONT_SIZE);
+      graphics2D.setFont(messageFont);
+      FontMetrics fontMetrics = graphics2D.getFontMetrics();
+      int messageWidth = fontMetrics.stringWidth(message); // Render the String centered
+      int messageHeight = fontMetrics.getAscent();
+      int whiteSpace = 10; // 10 pixels of white space around the message between the border
+      
+      // The white round filled rectangle to "clear" up the area
+      graphics2D.setColor(new Color(255, 255, 255, getFadeAlpha()));
+      graphics2D.fillRoundRect(middleX - messageWidth / 2 - whiteSpace,
+          middleY - messageHeight / 2 - whiteSpace, messageWidth + 2 * whiteSpace,
+          messageHeight + whiteSpace * 2, 12, 12);
+      
+      // The red round Border around the filled white rectangle
+      graphics2D.setColor(new Color(255, 0, 0, getFadeAlpha()));
+      graphics2D.drawRoundRect(middleX - messageWidth / 2 - whiteSpace,
+          middleY - messageHeight / 2 - whiteSpace, messageWidth + 2 * whiteSpace,
+          messageHeight + whiteSpace * 2, 12, 12);
+      
+      // The passed y-coordinate is the BOTTOM of the first Letter, not the top.
+      graphics2D.drawString(message, middleX - messageWidth / 2, middleY + messageHeight / 2 -
+          fontMetrics.getDescent() / 2);
+      
+      graphics2D.setColor(Color.BLACK); // restore the default color for further rendering
     }
   }
 
